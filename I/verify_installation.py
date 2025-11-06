b9e4ce960b101bda0cc7c3faac63b1aa8e04b354
ReyqiArdian <152360519+ReyqiArdian@users.noreply.github.com>
2025-11-06 11:23:01 +0700

    Add files via upload

---
+++ I/verify_installation.py
@@ -0,0 +1,11 @@
+import sys
+import numpy
+import pandas
+import matplotlib
+import sklearn
+
+print("Python version:", sys.version)
+print("NumPy version:", numpy.__version__)
+print("Pandas version:", pandas.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Scikit-learn version:", sklearn.__version__)
